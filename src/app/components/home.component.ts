ce023fadfc81267c99d991bfc402f867cd6c1311
Jesus Torres <is.jesustorres@gmail.com>
2017-11-20 00:51:11 -0600

    Cambios
    20/11/2017 - 12:51 am

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-home',
   templateUrl: '../views/home.html',
-  styleUrls: ['../styles/home.style.css']
+  styleUrls: ['../styles/home.style.css'],
 })
 export class HomeComponent {
+  ngOnInit() {
+    var elem = document.querySelector(".materialboxed");
+    var instance = new M.Materialbox(elem);
+  }
 
 }
