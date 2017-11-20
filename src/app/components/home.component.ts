c44af73a7e2f7db9ef65ec9541626f3677c7a2b3
Jesus Torres <is.jesustorres@gmail.com>
2017-11-20 02:44:59 -0600

    Index v1.1

--- src/app/components/home.component.ts
+++ src/app/components/home.component.ts
@@ -4,7 +4,7 @@ import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 @Component({
   selector: 'app-home',
   templateUrl: '../views/home.html',
-  styleUrls: ['../styles/home.style.css'],
+  styleUrls: ['../styles/home.style.css']
 })
 export class HomeComponent {
   // tslint:disable-next-line:use-life-cycle-interface
